5597830513c0128b55c04a1d7220c45a02bbc159
Pulin Borua <137756919+pulinborua@users.noreply.github.com>
2023-09-01 23:11:17 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+
+  $('.show').hide();
+
+
+  $('.hide').click(function(){
+    $('.continar').slideUp();
+    $('.hide').hide();
+    $('.show').show();
+});
+
+
+  $('.show').click(function(){
+    $('.continar').slideDown();
+    $('.hide').show();
+    $('.show').hide();
+});
+
+
+
+
+   
+
+
+   $('.button-1').click(function(){
+   $('.continar-2').toggleClass('abc');
+    
+});
+
+
+  
